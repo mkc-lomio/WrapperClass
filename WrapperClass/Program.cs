0c6a24a225bbbb36d630b5c0efec839040ec23ca
mlomio <44965214+DrMkcTheHandSome@users.noreply.github.com>
2021-08-22 16:42:08 +0800

    [ADD] Wrapper Class

---
+++ WrapperClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WrapperClass
+{
+    class Salary //Wrapper Class
+    {
+        class Tax //Inner Class
+        {
+            public int CalculateTax(int Val)
+            {
+                return Val - 10;
+            }
+        }
+
+        public int GetSalary()
+        {
+            Salary.Tax wpr = new Salary.Tax(); // Internally call to Wrapper class.
+            return wpr.CalculateTax(100);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Salary w = new Salary();
+            Console.WriteLine("Your salary is " + w.GetSalary());
+            Console.ReadLine();
+        }
+    }
+}
